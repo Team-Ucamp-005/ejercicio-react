5ee82ad3a05559543e1785e5c9c2a8a44a03ce3f
Alfredo Hernandez Noriega <alfredo.hernandez@kushkipagos.com>
2022-05-14 13:32:49 -0500

    function form

--- src/App.js
+++ src/App.js
@@ -6,15 +6,41 @@ import Hello from './components/Hello/Hello';
 import InputText from './components/InputText/InputText';
 import ColorInput from './components/ColorInput/ColorInput';
 import InputSize from './components/InputSize/InputSize';
+import FormPatients from './components/FormPatients/FormPatients';
 
 
 
 
 function App() {
 
-  const [text, setText] = useState('hola')
-  const [color, setColor] = useState('#000000')
-  const [size, setSize] = useState('16')
+  // const [text, setText] = useState('hola')
+  // const [color, setColor] = useState('#000000')
+  // const [size, setSize] = useState('16')
+
+  const pacientes = [
+    {
+      nombre: 'maria',
+      edad: 26,
+      altura: 160
+    },
+    {
+      nombre: 'Fernanda',
+      edad: 30,
+      altura: 180
+    },
+    {
+      nombre: 'Alejandra',
+      edad: 26,
+      altura: 150
+    },
+    {
+      nombre: 'Antonio',
+      edad: 29,
+      altura: 190
+    },
+  ]
+
+  const [patients, setPatients] = useState([])
 
   return (
     <div className="App">
@@ -36,6 +62,22 @@ function App() {
       <InputSize sizeValue={setSize} />
       <p >Texto</p>
       <InputText textValue={setText} /> */}
+
+
+      {
+        pacientes.map((paciente, i) => {
+          return (
+            <div key={i}>
+              <h3>{paciente.nombre}</h3>
+              <p> {paciente.edad} </p>
+              <p>{paciente.altura}</p>
+            </div>
+          )
+        })
+      }
+
+
+      <FormPatients />
 
     </div>
   );
